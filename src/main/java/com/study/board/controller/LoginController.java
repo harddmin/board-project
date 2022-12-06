282c4198678b7e270ec7c4c35f2571f63d9c5e29
JIMIN LEE <jimin1222@naver.com>
2022-12-06 20:04:30 +0900

    commit

---
+++ src/main/java/com/study/board/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.study.board.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+    private final PasswordEncoder passwordEncoder;
+
+}
