a3e48e23efd4db45ff4448306db0c9ec7f32e075
JIMIN LEE <jimin1222@naver.com>
2023-01-08 20:47:16 +0900

    commit

---
+++ src/main/java/com/study/board/entity/User.java
@@ -0,0 +1,19 @@
+package com.study.board.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
